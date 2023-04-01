0a3fd46a0b52c53833230af2ec631be9d78cf4ea
Tabriz Habiyev <91114214+TabrizHabiyev@users.noreply.github.com>
2023-04-01 14:39:38 +0400

    feat: Edu Partners CRUD completed

---
+++ src/EducationWebApi.Application/Features/Partners/Queries/GetAllPartnersQuery.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EducationWebApi.DataAccess.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationWebApi.Application.Features;
+
+public class GetAllPartnersQuery : IRequest<List<GetAllPartnersQueryDto>> { }
+
+public class GetAllPartnersQueryHandler : IRequestHandler<GetAllPartnersQuery, List<GetAllPartnersQueryDto>>
+{
+    private readonly IDatabaseContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAllPartnersQueryHandler(IDatabaseContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<GetAllPartnersQueryDto>> Handle(GetAllPartnersQuery query, CancellationToken cancellationToken)
+    {
+        return await _context.Partners
+        .Include(i => i.PartnersImage)
+        .ProjectTo<GetAllPartnersQueryDto>(_mapper.ConfigurationProvider)
+        .ToListAsync();
+    }
+}
